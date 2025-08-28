b41d926b2f4bb5d3caf6dd3bfb91f900991dfb32
Angela <angelamedina03.2016@gmail.com>
2025-08-27 19:15:45 -0500

    class nodo

--- paquetes.js
+++ paquetes.js
@@ -4,12 +4,3 @@ class nodo {                                // Define una clase para representar
     this.siguiente = null;               // Puntero al siguiente nodo; inicia en null porque aún no enlaza con nadie
   }
 }
-// Clase Cola (Queue) basada en lista enlazada
-class ColaPaquetes {                         // Define la estructura de la cola que gestionará los paquetes
-    constructor() {                         // Constructor de la cola
-    this.inicio = null;                   // Referencia al primer nodo (cabeza de la cola); null indica cola vacía
-    this.final = null;                    // Referencia al último nodo (cola/tail); null indica cola vacía
-    this.tamaño = 0;                      // Contador de elementos en la cola para consultas O(1)
-  }
-  
-}                     
